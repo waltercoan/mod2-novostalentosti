4d36ec7fa4c8a877092dd56d8cf0a9527d40ae5e
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-04-19 15:38:21 -0300

    atributos e metodos

--- src/App.java
+++ src/App.java
@@ -4,23 +4,32 @@ public class App {
     public static void main(String[] args) throws Exception {
         //Criando uma variável do tipo Pessoa
         //Criando uma instância de objeto a partir da classe Pessoa
-        Pessoa zezinho = new Pessoa();
-        zezinho.nome = "Zezinho da Silva Sauro";
+
+        //passagem de parametros
+        //instanciação
+        Pessoa zezinho = new Pessoa("Laranja");
+        zezinho.setNome("Zezinho da Silva Sauro");
         zezinho.CPF = "555.555.555-55";
         zezinho.altura = 1.8f;
         zezinho.idade = 22;
 
-        Pessoa luizinho = new Pessoa();
-        luizinho.nome = "Luizinho da Silva Sauro Jr";
+        Pessoa luizinho = new Pessoa("Limão");
+        //luizinho.nome = "Luizinho da Silva Sauro Jr";
 
-        System.out.println(zezinho.nome);
+        System.out.println(zezinho.getNome());
         System.out.println(zezinho.CPF);
         System.out.println(zezinho.altura);
         System.out.println(zezinho.idade);
-        System.out.println(luizinho.nome);
+        System.out.println(luizinho.getNome());
         System.out.println(luizinho.CPF);
         System.out.println(luizinho.idade);
-        
 
+        String guardaResultado = zezinho.podeDirigir();
+        System.out.println(guardaResultado);
+        guardaResultado = luizinho.podeDirigir();
+        System.out.println(guardaResultado);
+
+        zezinho.mostraCPF();
+        luizinho.mostraCPF();
     }
 }
--- src/introducao/Pessoa.java
+++ src/introducao/Pessoa.java
@@ -1,12 +1,38 @@
 package introducao;
 
-import java.util.ArrayList;
-
 public class Pessoa {
     //Atributos (variaveis) automaticamente inicializados com o valor padrão
-    public String nome;
+    //escopo de vida de instancia
+    private String nome;
     public int idade;
     public String CPF;
     public float altura;
     
+    //GETTER e SETTER
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    //comportamento
+    //CONSTRUTOR - inicializar um objeto e definir valores obrigatorios
+    public Pessoa(String nome){ //escopo de vida local
+        System.out.println("CONSTRUTOR:" + nome);
+        this.nome = nome;
+    }//ate aqui
+    
+    //metodo (função)
+    //assinatura de um método
+    //modificadordeacesso tipoderetorno nomedometodo(parametros) { corpo }
+    public String podeDirigir(){
+        if(this.idade >= 18){
+            return "Pode dirigir";
+        }else{
+            return "Não pode dirigir";
+        }
+    }
+    public void mostraCPF(){
+        System.out.println(this.CPF);
+    }
 }
