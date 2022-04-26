a67c2857f01ddaa94b5c51b39ba58f6db9f594b1
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-04-26 15:34:05 -0300

    menu swing

--- src/introducao/telas/Telinha1.java
+++ src/introducao/telas/Telinha1.java
@@ -4,14 +4,23 @@ import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 //BorderLayout
 public class Telinha1 extends JFrame {
     public Telinha1() {
 
-        JLabel texto = new JLabel("Eu não acredito...");
-        add(texto,"North");
+        JMenuBar barraMenu = new JMenuBar();
+        JMenu menu = new JMenu("Arquivo");
+        JMenuItem menuItem = new JMenuItem("Abrir");
+        menu.add(menuItem);
+        barraMenu.add(menu);
+        setJMenuBar(barraMenu);
+        //JLabel texto = new JLabel("Eu não acredito...");
+        //add(texto,"North");
         JButton botao = new JButton("Me clica!!!");
         add(botao,"South");
         JPanel painel = new JPanel();
