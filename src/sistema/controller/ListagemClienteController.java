e64d7f40534c312ad9f9d0719e63e6cada8c2c7b
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-04-27 17:27:03 -0300

    construcao de um crud - observer

---
+++ src/sistema/controller/ListagemClienteController.java
@@ -0,0 +1,14 @@
+package sistema.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
+
+public class ListagemClienteController implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JOptionPane.showMessageDialog(null, "EU NAO ACREDITOOO!!!!!");
+    }
+    
+}
--- src/sistema/view/ListagemCliente.java
+++ src/sistema/view/ListagemCliente.java
@@ -2,13 +2,25 @@ package sistema.view;
 
 import java.awt.Color;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import sistema.controller.ListagemClienteController;
+
+import java.awt.FlowLayout;
 
 public class ListagemCliente extends JFrame {
 
-    private JPanel jpnCentro = new JPanel();
+    private JScrollPane jpnCentro;
     private JPanel jpnSul = new JPanel();
+    private JButton btnNovo = new JButton("Novo");
+    private JButton btnAlterar = new JButton("Alterar");
+    private JButton btnExcluir = new JButton("Excluir");
+    private JTable tabela = new JTable();
+    private ListagemClienteController controller = new ListagemClienteController();
 
     public ListagemCliente() {
         setSize(500,400);
@@ -17,8 +29,23 @@ public class ListagemCliente extends JFrame {
         setVisible(true);
     }
     private void criaPaineis(){
-        add(jpnCentro,"Center");
-        add(jpnSul,"South");
+        //import java.awt.FlowLayout;
+        jpnSul.setLayout(new FlowLayout(FlowLayout.LEFT));
         
+        
+        add(jpnSul,"South");
+        jpnSul.add(btnNovo);
+        jpnSul.add(btnAlterar);
+        jpnSul.add(btnExcluir);
+        
+        btnNovo.addActionListener(controller);
+        btnAlterar.addActionListener(controller);
+        btnExcluir.addActionListener(controller);
+
+        jpnCentro = new JScrollPane(tabela);
+        jpnCentro.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        jpnCentro.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        add(jpnCentro,"Center");
     }
 }
