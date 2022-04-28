942e7564dd2e2efcf57b25d6cecabd4dc9f38066
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-04-28 15:31:28 -0300

    implementacao do listener dos botoes da tela

--- src/sistema/controller/ListagemClienteController.java
+++ src/sistema/controller/ListagemClienteController.java
@@ -2,27 +2,36 @@ package sistema.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JOptionPane;
 
 public class ListagemClienteController implements ActionListener{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        JButton botaoClicado = (JButton)e.getSource();
+        JComponent botaoClicado = (JComponent)e.getSource();
         switch (botaoClicado.getName()) {
             case "btnNovo":
-                JOptionPane.showMessageDialog(null, "BOTAO NOVO");    
+                btnNovoClique();
             break;
             case "btnAlterar":
-                JOptionPane.showMessageDialog(null, "BOTAO ALTERAR");    
+                btnAlterarClique();
             break;
-       
-            default:
-                break;
+            case "btnExcluir":
+                btnExcluirClique();
+            break;
         }
-        
     }
-    
+
+    private void btnNovoClique(){
+        JOptionPane.showMessageDialog(null, "BOTAO NOVO");    
+    }
+    private void btnAlterarClique(){
+        JOptionPane.showMessageDialog(null, "BOTAO ALTERAR");    
+    }
+    private void btnExcluirClique(){
+        JOptionPane.showMessageDialog(null, "BOTAO Excluir");
+    }
+
+
 }
