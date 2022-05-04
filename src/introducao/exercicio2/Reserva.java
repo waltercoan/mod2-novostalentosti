f73c602bda60a19f415b4e815e60d4af3abfff46
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-05-04 14:08:22 -0300

    exercicio 02

--- src/introducao/exercicio2/Reserva.java
+++ src/introducao/exercicio2/Reserva.java
@@ -1,12 +1,31 @@
 package introducao.exercicio2;
 
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.GregorianCalendar;
 
 public class Reserva {
     private int numero;
     private Date dataInicial;
     private Date dataFinal;
+    private Quarto quarto;
+    private ArrayList<Hospede> listaHospedes = new ArrayList<>();
+
+
+    public Quarto getQuarto() {
+        return quarto;
+    }
+
+    public ArrayList<Hospede> getListaHospedes() {
+        return listaHospedes;
+    }
+
+    public void setListaHospedes(ArrayList<Hospede> listaHospedes) {
+        this.listaHospedes = listaHospedes;
+    }
+
+    public void setQuarto(Quarto quarto) {
+        this.quarto = quarto;
+    }
 
     public Reserva(int numero) {
         this.numero = numero;
