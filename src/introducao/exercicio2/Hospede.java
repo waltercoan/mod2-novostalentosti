e6256c6042744ef6f4f505eff61f42fa668db4d2
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-05-04 14:00:46 -0300

    exercicio 02

--- src/introducao/exercicio2/Hospede.java
+++ src/introducao/exercicio2/Hospede.java
@@ -1,5 +1,37 @@
 package introducao.exercicio2;
 
 public class Hospede {
+    private String nome;
+    private String CPF;
+    private String telefone;
+
+    public Hospede(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String cPF) {
+        CPF = cPF;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
     
 }
--- src/introducao/exercicio2/Quarto.java
+++ src/introducao/exercicio2/Quarto.java
@@ -3,7 +3,12 @@ package introducao.exercicio2;
 public class Quarto {
     private int numero;
 
-    public Quarto() {
+    public int getNumero() {
+        return numero;
     }
-    
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
 }
