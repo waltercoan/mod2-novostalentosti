9f8a2e8ba647b0818fa3fc932ae053889b39140f
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-05-11 18:02:33 -0300

    exercicio 4

--- src/Sorteador.java
+++ src/Sorteador.java
@@ -2,11 +2,14 @@ import java.util.Random;
 
 public class Sorteador{
     public static void main(String[] args) {
-        Random sorteador = new Random();
+
+        String texto = "abcZ";
+        System.out.println(texto.matches("[a-zA-Z]+"));
+        /*Random sorteador = new Random();
         sorteador.setSeed(System.currentTimeMillis());
         for(int i=0;i<=1;i++){
             var dado = sorteador.nextInt(1000);
             System.out.println(dado);
-        }
+        }*/
     }
 }
--- src/introducao/App7.java
+++ src/introducao/App7.java
@@ -5,10 +5,11 @@ import java.util.GregorianCalendar;
 import introducao.exercicio4.EmpresaCliente;
 import introducao.exercicio4.Funcionario;
 import introducao.exercicio4.PessoaFisica;
-import introducao.exercicio4.Pessoa;
+import introducao.exercicio4.PessoaJuridica;
 
 public class App7 {
     public static void main(String[] args) {
+        //Instanciando a classe funcionario e a classe empresacliente
         Funcionario funcionario1 = new Funcionario();
         EmpresaCliente empresa1 = new EmpresaCliente();
 
@@ -37,7 +38,56 @@ public class App7 {
         System.out.println(pessoa1.getNome());
         System.out.println(pessoa1.getTelefone());
 
-        Pessoa pessoa3;
+        //3)Crie uma variável pessoa2:PessoaJuridica e 
+        PessoaJuridica pessoa2;
+        //atribua o valor de empresa1. Imprima em 
+        pessoa2 = empresa1;
+        //tela os atributos que são visíveis.
+        System.out.println(pessoa2.getNome());
+        System.out.println(pessoa2.getTelefone());
+        System.out.println(pessoa2.getDataCriacao());
+        System.out.println(pessoa2.getCNPJ());
+        
+        //4)Crie uma variável pessoa3:Pessoa e 
+        introducao.exercicio4.Pessoa pessoa3;
+        //atribua o valor de pessoa1. Imprima 
+        pessoa3 = pessoa1;
+        //em tela os atributos que são visíveis.
+        System.out.println(pessoa3.getNome());
+        System.out.println(pessoa3.getTelefone());
+
+        //5)Crie uma variável pessoa4:Pessoa e 
+        introducao.exercicio4.Pessoa pessoa4;
+        //atribua o valor de pessoa2. Imprima
+        pessoa4 = pessoa2; 
+        //em tela os atributos que são visíveis.
+        System.out.println(pessoa4.getNome());
+        System.out.println(pessoa4.getTelefone());
+        
+        //6)Crie uma variável funcionario2:Funcionario 
+        Funcionario funcionario2;
+        //e atribua o valor de pessoa3. Imprima 
+        funcionario2 = (Funcionario)pessoa3;
+        //em tela os atributos que são visíveis.
+        System.out.println(funcionario2.getNome());
+        System.out.println(funcionario2.getCPF());
+        System.out.println(funcionario2.getDataNascimento());
+        System.out.println(funcionario2.getFuncao());
+        System.out.println(funcionario2.getId());
+        System.out.println(funcionario2.getTelefone());
+
+        //7)Crie uma variável empresa2:EmpresaCliente e 
+        EmpresaCliente empresa2;
+        //atribua o valor de pessoa4. 
+        //Imprima em tela os atributos que são visíveis.
+        empresa2 = (EmpresaCliente)pessoa4;
+        System.out.println(empresa2.getNome());
+        System.out.println(empresa2.getCNPJ());
+        System.out.println(empresa2.getDataCriacao());
+        System.out.println(empresa2.getMarca());
+        System.out.println(empresa2.getNumFuncionarios());
+        System.out.println(empresa2.getTelefone());
+        
 
     }
 }
