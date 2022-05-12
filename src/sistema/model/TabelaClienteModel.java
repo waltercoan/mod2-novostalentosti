5d1cbacfff1e0dfa3177ff2b0a502ac69055367d
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-05-12 15:31:21 -0300

    listagem clientes

--- src/sistema/model/TabelaClienteModel.java
+++ src/sistema/model/TabelaClienteModel.java
@@ -1,5 +1,7 @@
 package sistema.model;
 
+import java.text.SimpleDateFormat;
+
 import javax.swing.table.AbstractTableModel;
 
 import sistema.controller.ListagemClienteController;
@@ -22,6 +24,7 @@ public class TabelaClienteModel extends AbstractTableModel {
     public int getColumnCount() {
         return 4;
     }
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         var umCliente = controller.getAllClientes().get(rowIndex);
@@ -30,8 +33,18 @@ public class TabelaClienteModel extends AbstractTableModel {
                 return umCliente.getId();                
             case 1:
                 return umCliente.getNome();
+            case 2:
+                return umCliente.getCPF();
+            case 3:
+                return sdf.format(umCliente.getDataNascimento());
+        
             default:
                 return "";
         }
     }
+    @Override
+    public String getColumnName(int column) {
+        return new String[]{"Código", "Nome", "CPF", "Data Nascimento"}[column];
+    }
+    
 }
