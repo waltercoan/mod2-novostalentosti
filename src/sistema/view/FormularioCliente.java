dead5d2354e71457f78eda8fa20985c2790557b9
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-05-17 17:01:18 -0300

    formulario gridlayout

--- src/sistema/view/FormularioCliente.java
+++ src/sistema/view/FormularioCliente.java
@@ -2,10 +2,14 @@ package sistema.view;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.text.MaskFormatter;
+
 import java.awt.GridBagLayout;
+import java.text.ParseException;
 import java.awt.GridBagConstraints;
 
 public class FormularioCliente extends JDialog{
@@ -15,6 +19,11 @@ public class FormularioCliente extends JDialog{
     private JButton btnCancelar = new JButton("Cancelar");
     private JTextField txtId = new JTextField(4);
     private JTextField txtNome = new JTextField(40);
+    private MaskFormatter mascaraCPF;
+    private JFormattedTextField txtCPF;
+    private MaskFormatter mascaraData;
+    private JFormattedTextField txtData;
+    
     public FormularioCliente() {
         setSize(400,300);
         setModal(true); //bloquear o usuário na JDialog
@@ -50,5 +59,42 @@ public class FormularioCliente extends JDialog{
         cons.gridy = 1; //linha 1
         cons.weightx = 0.80; //largura em percentual
         jpnCentro.add(txtNome,cons);
+
+        cons.gridx = 0; //coluna 0
+        cons.gridy = 2; //linha 2
+        cons.weightx = 0.20; //largura em percentual
+        jpnCentro.add(new JLabel("CPF:"),cons);
+
+
+        try {
+            mascaraCPF = new MaskFormatter("###.###.###-##");
+            txtCPF = new JFormattedTextField(mascaraCPF);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        cons.gridx = 1; //coluna 1
+        cons.gridy = 2; //linha 2
+        cons.weightx = 0.80; //largura em percentual
+        jpnCentro.add(txtCPF,cons);
+
+
+        cons.gridx = 0; //coluna 0
+        cons.gridy = 3; //linha 3
+        cons.weightx = 0.20; //largura em percentual
+        jpnCentro.add(new JLabel("Data Nascimento:"),cons);
+
+        try {
+            mascaraData = new MaskFormatter("##/##/####");
+            txtData = new JFormattedTextField(mascaraData);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        cons.gridx = 1; //coluna 1
+        cons.gridy = 3; //linha 3
+        cons.weightx = 0.80; //largura em percentual
+        jpnCentro.add(txtData,cons);
+
     }
 }
