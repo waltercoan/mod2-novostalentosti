6f76fb4cf549f127582e73edef3c9df001e8807b
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-05-24 15:17:49 -0300

    exercicio7

--- src/introducao/App10.java
+++ src/introducao/App10.java
@@ -1,10 +1,28 @@
 package introducao;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import introducao.exercicio7.ContaDeLuz;
 import introducao.exercicio7.Planilha;
 
 public class App10 {
     public static void main(String[] args) {
+        Calendar calendario = GregorianCalendar.getInstance();
         Planilha plan1 = new Planilha();
+
+        calendario.set(2022, 05, 24);
+        ContaDeLuz conta1 = 
+            new ContaDeLuz(300f, calendario.getTime(), 
+                          1, 32, 
+                          calendario.getTime(), 0);
         
+        calendario.set(2022, 05, 23);
+        ContaDeLuz conta2 = 
+            new ContaDeLuz(400f, calendario.getTime(), 
+                        2, 42, 
+                        calendario.getTime(), 0);
+        plan1.getListaContasDeLuz().add(conta1);
+        plan1.getListaContasDeLuz().add(conta2);
     }
 }
--- src/introducao/exercicio7/Planilha.java
+++ src/introducao/exercicio7/Planilha.java
@@ -1,8 +1,40 @@
 package introducao.exercicio7;
 
+import java.util.ArrayList;
+
 public class Planilha {
+
+    private ArrayList<ContaDeLuz> listaContasDeLuz = new ArrayList<>();
+    
+    public ArrayList<ContaDeLuz> getListaContasDeLuz() {
+        return listaContasDeLuz;
+    }
+
+    public void setListaContasDeLuz(ArrayList<ContaDeLuz> listaContasDeLuz) {
+        this.listaContasDeLuz = listaContasDeLuz;
+    }
     public float calcularUltimoValorMedio(){
-        return 0;
+        float soma = 0;
+        float media = 0;
+        for(int i=0;i<listaContasDeLuz.size();i++){
+            ContaDeLuz umaConta = listaContasDeLuz.get(i);
+            soma = soma + umaConta.getValorDaConta();
+        }
+        media = soma / listaContasDeLuz.size();
+        return media;
+    }
+    public float calcularUltimoValorMediov2(){
+        float soma = 0;
+        float media = 0;
+        for(ContaDeLuz umaConta : listaContasDeLuz){
+            soma = soma + umaConta.getValorDaConta();
+        }
+        media = soma / listaContasDeLuz.size();
+        return media;
+    }
+    public float calcularUltimoValorMediov3(){
+        return (float)listaContasDeLuz.stream().mapToDouble(a -> a.getValorDaConta()).sum()
+        / listaContasDeLuz.size();
     }
 
     public float calcularMaiorValor(){
