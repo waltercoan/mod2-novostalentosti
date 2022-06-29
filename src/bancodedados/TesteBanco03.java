243ac7a821831a15ae921e67acf4c4ae443cd477
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-06-29 17:08:49 -0300

    banco de dados

--- src/bancodedados/TesteBanco03.java
+++ src/bancodedados/TesteBanco03.java
@@ -5,20 +5,21 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class TesteBanco02 {
-   public static void main(String[] args) {
+public class TesteBanco03 {
+    public static void main(String[] args) {
         var stringConnexao = "jdbc:mariadb://localhost/dbexerc1";
         var usuario = "root";
         var senha = "univille";
         try (Connection conn = DriverManager.getConnection(stringConnexao, usuario, senha)) {
-            var sql = "insert into aluno(nome) values(?)";
+            var sql = "update aluno set nome = ? where numero = ?";
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, "Maga Patalógica");
+            ps.setString(1, "Maga Pata da Silva");
+            ps.setInt(2, 10);
             var numlinhasafetadas = ps.executeUpdate();
-            System.out.println("Numero de linhas afetadas: " + numlinhasafetadas);
-            
+            System.out.println("Total de linhas afetadas: " + numlinhasafetadas);
         }catch (SQLException e) {
             e.printStackTrace();
         }
-   } 
+
+    }
 }
