a9ea2bf80089348977e7c05e08b3707391806093
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-06-29 16:25:34 -0300

    banco de dados

--- src/bancodedados/TesteBanco01.java
+++ src/bancodedados/TesteBanco01.java
@@ -2,9 +2,12 @@ package bancodedados;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import javax.swing.JOptionPane;
 
 public class TesteBanco01{
     public static void main(String[] args) {
@@ -12,15 +15,31 @@ public class TesteBanco01{
         var stringConnexao = "jdbc:mariadb://localhost/dbexerc1";
         var usuario = "root";
         var senha = "univille";
+        //Abre a conexão com o banco de dados
         try (Connection conn = DriverManager.getConnection(stringConnexao, usuario, senha)) {
-            var sql = "select * from aluno";
-            try(Statement stmt = conn.createStatement()){
+            //var sql = "select * from aluno";
+            //var sql = "select * from aluno where nome like '%nho'";
+            var busca = JOptionPane.showInputDialog(null, "Digite o nome procurado");
+            //var sql = "select * from aluno where nome like '%" + busca +"%'";
+            //System.out.println(sql);
+            var sql = "select * from aluno where nome like ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, "%"+busca+"%");
+            try(ResultSet rs = ps.executeQuery()){
+                while(rs.next()){
+                    System.out.print(rs.getString("numero"));
+                    System.out.println(rs.getString("nome"));
+                }
+            }
+            /*try(Statement stmt = conn.createStatement()){
+                //Executando query no banco de dados
                 try(ResultSet rs = stmt.executeQuery(sql)){
                     while(rs.next()){
+                        System.out.print(rs.getString("numero"));
                         System.out.println(rs.getString("nome"));
                     }
                 }
-            }
+            }*/
         } catch (SQLException e) {
             e.printStackTrace();
         }
---
+++ src/bancodedados/TesteBanco02.java
@@ -0,0 +1,10 @@
+package bancodedados;
+
+public class TesteBanco02 {
+   public static void main(String[] args) {
+      var stringConnexao = "jdbc:mariadb://localhost/dbexerc1";
+      var usuario = "root";
+      var senha = "univille";
+      
+   } 
+}
